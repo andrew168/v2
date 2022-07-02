5daab2d3044df64695c06b46ba0170eb090751dd
andrew <andrewzhi168@hotmail.com>
2022-07-02 18:55:00 -0400

    DescriptorSetLayout的CreateInfo已经内部化，用ci简称

--- auxVk/descriptorSetLayout.cpp
+++ auxVk/descriptorSetLayout.cpp
@@ -5,11 +5,11 @@ namespace aux
 DescriptorSetLayout::DescriptorSetLayout(
 	std::vector<VkDescriptorSetLayoutBinding> &dslBindings)
 {
-	VkDescriptorSetLayoutCreateInfo descriptorSetLayoutCI{};
-	descriptorSetLayoutCI.sType = VK_STRUCTURE_TYPE_DESCRIPTOR_SET_LAYOUT_CREATE_INFO;
-	descriptorSetLayoutCI.pBindings = dslBindings.data();
-	descriptorSetLayoutCI.bindingCount = static_cast<uint32_t>(dslBindings.size());
-	VK_CHECK_RESULT(vkCreateDescriptorSetLayout(Device::getR(), &descriptorSetLayoutCI, nullptr, &m_dsLayout));
+	VkDescriptorSetLayoutCreateInfo ci{};
+	ci.sType = VK_STRUCTURE_TYPE_DESCRIPTOR_SET_LAYOUT_CREATE_INFO;
+	ci.pBindings = dslBindings.data();
+	ci.bindingCount = static_cast<uint32_t>(dslBindings.size());
+	VK_CHECK_RESULT(vkCreateDescriptorSetLayout(Device::getR(), &ci, nullptr, &m_dsLayout));
 }
 
 DescriptorSetLayout::~DescriptorSetLayout()
