38b7f5285ad8f7354c78d8511d8710f2a76292dd
andrew <andrewzhi168@hotmail.com>
2022-07-03 13:32:53 -0400

    Cmd Pool用缺省参数，支持多途径create 允许定义变量，不带参数

--- auxVk/commandPool.cpp
+++ auxVk/commandPool.cpp
@@ -2,13 +2,22 @@
 
 namespace aux
 {
-CommandPool::CommandPool(uint32_t queueFamilyIndex)
+CommandPool::CommandPool()
+{
+}
+
+CommandPool::CommandPool(uint32_t queueFamilyIndex, VkCommandPoolCreateFlags flags)
+{
+	create(queueFamilyIndex, flags);
+}
+
+void CommandPool::create(uint32_t queueFamilyIndex, VkCommandPoolCreateFlags flags)
 {
 	// Separate command pool as queue family for compute may be different than graphics
 	VkCommandPoolCreateInfo cmdPoolInfo = {};
 	cmdPoolInfo.sType = VK_STRUCTURE_TYPE_COMMAND_POOL_CREATE_INFO;
 	cmdPoolInfo.queueFamilyIndex = queueFamilyIndex;
-	cmdPoolInfo.flags = VK_COMMAND_POOL_CREATE_RESET_COMMAND_BUFFER_BIT;
+	cmdPoolInfo.flags = flags;
 	VK_CHECK_RESULT(vkCreateCommandPool(Device::getR(), &cmdPoolInfo, nullptr, &m_pool));
 }
 
--- auxVk/commandPool.h
+++ auxVk/commandPool.h
@@ -8,8 +8,11 @@ class CommandPool
 {
     VkCommandPool m_pool;
 public:
-    CommandPool(uint32_t queueFamilyIndex);
+    CommandPool();
+    CommandPool(uint32_t queueFamilyIndex, VkCommandPoolCreateFlags flags = VK_COMMAND_POOL_CREATE_RESET_COMMAND_BUFFER_BIT);
     ~CommandPool();
+
+    void create(uint32_t queueFamilyIndex, VkCommandPoolCreateFlags flags = VK_COMMAND_POOL_CREATE_RESET_COMMAND_BUFFER_BIT);
 
     VkCommandPool* get() { return &m_pool; }
     VkCommandPool& getR() { return m_pool; }
